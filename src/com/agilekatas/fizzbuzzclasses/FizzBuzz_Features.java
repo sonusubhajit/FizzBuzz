157c2fac708a10e89e073b290b523f7c2927783c
subhajit <karmakarsubhajit123@gmail.com>
2022-08-30 20:17:10 +0530

    FizzBuzz Feature 1_Second Test Case is successful

--- src/com/agilekatas/fizzbuzzclasses/FizzBuzz_Features.java
+++ src/com/agilekatas/fizzbuzzclasses/FizzBuzz_Features.java
@@ -3,6 +3,10 @@ package com.agilekatas.fizzbuzzclasses;
 public class FizzBuzz_Features {
 
 	public String getfizzbuzzresult(int number) {
+
+		if (number % 3 == 0) {
+			return "fizz";
+		}
 		return String.valueOf(number);
 	}
 
